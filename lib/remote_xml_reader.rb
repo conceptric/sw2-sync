c541b33f09afc8ec22b4b97ca415f1e05492cfe0
James Whinfrey <james@conceptric.co.uk>
2012-04-17 17:34:38 +0100

    Add a method to extract xml elements as an array of nokogiri elements.

--- lib/remote_xml_reader.rb
+++ lib/remote_xml_reader.rb
@@ -2,5 +2,12 @@ module RemoteXmlReader
   def RemoteXmlReader.fetch(remote_url)
     open(remote_url).read
   end
+
+  def RemoteXmlReader.extract_named_nodes(remote_url, node_name)
+    nodes = []
+    doc = Nokogiri::XML(self.fetch(remote_url))
+    doc.search(node_name).each {|item| nodes << item }
+    nodes
+  end
 end  
 
--- spec/remote_xml_reader_spec.rb
+++ spec/remote_xml_reader_spec.rb
@@ -11,10 +11,40 @@ describe RemoteXmlReader do
         with(test_url).
         and_return(
         StringIO.new(File.read(File.join(File.dirname(__FILE__), 
-          *%w[fixtures sw2_simple_example.xml])))
+          *%w[fixtures sw2_harder_example.xml])))
         )
       subject.fetch(test_url)  
     end    
   end  
+
+  describe ".extract_named_nodes" do
+    before(:each) do
+      subject.stub(:fetch).and_return(
+        open(FIXTURES + '/sw2_harder_example.xml'))      
+    end
+
+    it "returns an array of nodes matching the name" do
+      result = subject.extract_named_nodes('feed_url', 'item')
+      result.should be_instance_of(Array)
+      result.count.should == 2          
+    end                                                        
+    
+    it "each of these data nodes are Nokogiri elements" do
+      result = subject.extract_named_nodes('feed_url', 'item')
+      result.first.should be_instance_of(Nokogiri::XML::Element)      
+    end
+
+    it "each element contains the correct amount of data" do
+      result = subject.extract_named_nodes('feed_url', 'item')
+      result.first.children.size.should == 29
+      result.last.children.size.should == 29
+    end
+
+    it "each element contains the correct data" do
+      result = subject.extract_named_nodes('feed_url', 'item')
+      result.first.search('reference').text.should == '06468'
+      result.last.search('reference').text.should == '06469'
+    end
+  end
 end
   
