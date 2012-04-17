2be11fa27dcd7dde53921769b9dfbe5103f472c2
James Whinfrey <james@conceptric.co.uk>
2012-04-17 16:46:47 +0100

    Cleans up the presentation in the module spec.

--- spec/remote_xml_reader_spec.rb
+++ spec/remote_xml_reader_spec.rb
@@ -1,14 +1,18 @@
 require File.join(File.dirname(__FILE__), *%w[.. lib remote_xml_reader.rb])
 
 describe RemoteXmlReader do
+
   let(:test_url) { 'http://www.job-tv.co.uk/XML.asp' }
   
   describe ".fetch" do
     it "opens a remote url and reads the contents" do
-      subject.should_receive(:open).once.with(test_url).
-        and_return(StringIO.new(
-        File.read(File.join(File.dirname(__FILE__), 
-          *%w[fixtures sw2_simple_example.xml]))))
+      subject.should_receive(:open).
+        once.
+        with(test_url).
+        and_return(
+        StringIO.new(File.read(File.join(File.dirname(__FILE__), 
+          *%w[fixtures sw2_simple_example.xml])))
+        )
       subject.fetch(test_url)  
     end    
   end  
