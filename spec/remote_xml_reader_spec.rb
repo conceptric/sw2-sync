63c3e93d071e60d76366de62ad5dbfe6446d477a
James Whinfrey <james@conceptric.co.uk>
2012-04-18 19:45:24 +0100

    Refactors the specs, reduced repetition.

--- spec/remote_xml_reader_spec.rb
+++ spec/remote_xml_reader_spec.rb
@@ -7,15 +7,21 @@ end
 
 describe RemoteXmlReader do
 
-  let(:test_url) { 'http://www.example.com/example.xml' }
-  
   describe "RemoteXmlReader.open" do
+    
     it "opens a remote url" do
-      RemoteXmlReader.should_receive(:open).once.and_return(
+      test_url = 'http://www.example.com/example.xml'
+
+      RemoteXmlReader.should_receive(:open).
+      once.
+      with(test_url).
+      and_return(
         fixture_stream_helper('single_node.xml'))
+
       RemoteXmlReader.open(test_url).read.
           should == open(FIXTURES + '/single_node.xml').read  
-    end    
+    end
+        
   end  
 
   describe ".new" do      
@@ -34,77 +40,75 @@ describe RemoteXmlReader do
       xml_file = open(FIXTURES + '/single_node.xml')
       RemoteXmlReader.new('remote_url').read.
         should == xml_file.read
-    end        
+    end
+    
   end
 
   describe ".extract_named_nodes" do
+    
     before(:each) do
       RemoteXmlReader.stub(:open).and_return(
         open(FIXTURES + '/twin_node.xml'))      
-      @remote_xml_reader = RemoteXmlReader.new('remote_url')
+      remote_xml_reader = RemoteXmlReader.new('remote_url')
+      @result = remote_xml_reader.extract_named_nodes('item')
     end
   
     it "returns an array containing matching nodes" do
-      result = @remote_xml_reader.extract_named_nodes('item')
-      result.size.should == 2
+      @result.size.should == 2
     end                                                        
     
     it "each of these data nodes are Nokogiri elements" do
-      result = @remote_xml_reader.extract_named_nodes('item')
-      result.each do |item|
+      @result.each do |item|
         item.should be_instance_of(Nokogiri::XML::Element)
       end
     end
   
     it "each element contains the correct amount of data" do
-      result = @remote_xml_reader.extract_named_nodes('item')
-      result.each do |item|
+      @result.each do |item|
         item.children.count.should == 5
       end
     end
   
     it "each element contains the correct data" do
-      result = @remote_xml_reader.extract_named_nodes('item')
-      result.each do |item|
+      @result.each do |item|
         item.node_name == "item"
         item.first_element_child.node_name.should == 'child1'
         item.last_element_child.node_name.should == 'child2'
       end
     end
+    
   end 
   
   describe ".named_nodes_to_hash" do
-    before(:each) do
+    
+    def setup(actual_xml_file)
       RemoteXmlReader.stub(:open).and_return(
-        open(FIXTURES + '/single_node.xml'))      
+        open(FIXTURES + '/' + actual_xml_file))      
       remote_reader = RemoteXmlReader.new("remote_url")
-      @result = remote_reader.named_nodes_to_hash("item")      
+      remote_reader.named_nodes_to_hash("item")
     end
 
     it "converts the children of a named node to a hash" do 
-      @result.should == [{child1: "value1", child2: "value2"}]
+      result = setup('single_node.xml')
+      result.should == [{child1: "value1", child2: "value2"}]
     end
 
     it "converts the children of two named node to a hashes" do 
-      RemoteXmlReader.stub(:open).and_return(
-        open(FIXTURES + '/twin_node.xml'))      
-      remote_reader = RemoteXmlReader.new("remote_url")
-      result = remote_reader.named_nodes_to_hash("item")
+      result = setup('twin_node.xml')
       result.should == [
         {child1: "value1", child2: "value2"},
         {child1: "value1", child2: "value2"}]
     end
 
     it "the hash is not recursive" do 
-      RemoteXmlReader.stub(:open).and_return(
-        open(FIXTURES + '/complex_twin_node.xml'))      
-      remote_reader = RemoteXmlReader.new("remote_url")
-      result = remote_reader.named_nodes_to_hash("item")
+      result = setup('complex_twin_node.xml')
       result.should == [
         {child1: "value1", child2: "value2"},
         {child1: "value1", child2: "CDATA is included\nwith special characters"},
         {child1: "value1", child2: "value2"}]
-    end
+    end                           
+    
   end
+  
 end
   
