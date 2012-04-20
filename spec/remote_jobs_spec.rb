f7ee82dca9578b0c10b57bb5cdade88acd9b142e
James Whinfrey <james@conceptric.co.uk>
2012-04-20 14:02:18 +0100

    Extends the job finding behaviour. This is about as far as I want to go with this aspect, it is really not a core part of the implementation.

--- spec/remote_jobs_spec.rb
+++ spec/remote_jobs_spec.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__), *%w[.. lib remote_jobs.rb])
 
-class Job
+class MockJob
   include RemoteJobs
   
   def self.find_with_reference
@@ -8,18 +8,38 @@ class Job
   end
 end
 
+class MockNoJob
+  include RemoteJobs
+  
+  def self.find_with_reference
+    []
+  end
+end
+
 describe RemoteJobs do
-
+  
   describe ".find_jobs_to_sync" do    
-    it "return an array" do      
-      Job.find_jobs_to_sync.should be_instance_of Array
+    subject { MockJob.find_jobs_to_sync }
+    
+    it "returns an array" do      
+      subject.should be_instance_of Array
+    end
+    
+    context "when there are jobs to sync" do
+      it "of jobs to be synchronised with the remote source" do            
+        subject.size.should_not == 0
+        subject.each {|job| job.should be_instance_of(MockJob)}
+      end
     end
 
-    it "of jobs to be synchronised with the remote source" do      
-      jobs = Job.find_jobs_to_sync
-      jobs.size.should_not == 0
-      jobs.each {|job| job.should be_instance_of(Job)}
+    context "when there aren't any jobs to sync" do
+      subject { MockNoJob.find_jobs_to_sync }
+      
+      it "of jobs to be synchronised with the remote source" do            
+        subject.size.should == 0
+      end
     end
+
   end  
  
 end
