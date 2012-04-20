5fc9c949573a35d41db605251fb82c360278ec0b
James Whinfrey <james@conceptric.co.uk>
2012-04-20 14:58:16 +0100

    Adds an interface for the remote xml reader.

--- lib/remote_jobs.rb
+++ lib/remote_jobs.rb
@@ -1,8 +1,14 @@
+require 'remote_xml_reader'
+
 module RemoteJobs
   module ClassMethods
     def find_jobs_to_sync(&block)
       yield
     end    
+    
+    def find_remote_jobs(remote_url)
+      RemoteXmlReader.new(remote_url).child_nodes_to_hash('jobs')
+    end
   end
   
   module InstanceMethods
--- spec/remote_jobs_spec.rb
+++ spec/remote_jobs_spec.rb
@@ -11,11 +11,23 @@ end
 class MockNoJob
   def self.find
     []
-  end
+  end  
+end
+
+class MockRemoteXmlReader  
+  def child_nodes_to_hash(node_name)
+    [{reference:'1', title: 'job title'}]
+  end  
+end
+
+class MockEmptyRemoteXmlReader  
+  def child_nodes_to_hash(node_name)
+    []
+  end  
 end
 
 describe RemoteJobs do
-  
+
   describe ".find_jobs_to_sync" do    
     subject { MockJob.find_jobs_to_sync { MockJob.find } }
     
@@ -26,18 +38,49 @@ describe RemoteJobs do
     context "when there are jobs to sync" do
       it "of jobs to be synchronised with the remote source" do            
         subject.size.should_not == 0
-        subject.each {|job| job.should be_instance_of(MockJob)}
+        subject.each {|job| job.should be_instance_of MockJob}
       end
     end
 
     context "when there aren't any jobs to sync" do
       subject { MockJob.find_jobs_to_sync  { MockNoJob.find } }
       
-      it "of jobs to be synchronised with the remote source" do            
+      it "returns an empty array" do            
         subject.size.should == 0
       end
     end
+  end
+  
+  describe ".find_remote_jobs" do
 
+    before(:each) do 
+      RemoteXmlReader.stub(:new).with('remote_url').
+        and_return(MockRemoteXmlReader.new)            
+    end
+
+    subject { MockJob.find_remote_jobs('remote_url') }
+        
+    it "takes a remote url and returns an array" do
+      subject.should be_instance_of Array 
+    end
+
+    context "when there are remote jobs" do
+      it "of jobs to be synchronised with the local database" do            
+        subject.size.should_not == 0
+        subject.each {|job| job.should be_instance_of Hash}
+      end
+    end
+
+    context "when there aren't any remote jobs" do     
+      before(:each) do 
+        RemoteXmlReader.stub(:new).with('remote_url').
+          and_return(MockEmptyRemoteXmlReader.new)            
+      end
+
+      it "returns an empty array" do            
+        subject.size.should == 0
+      end
+    end    
   end  
  
 end
